5af5321da0a132e8e3c731468a3ab1cb1b1b1227
nua <enuesaa@gmail.com>
2024-03-06 00:42:02 +0900

    feat: show edit button

--- packages/fnui/src/components/common/Header.tsx
+++ packages/fnui/src/components/common/Header.tsx
@@ -1,13 +1,19 @@
-import { Container, Separator } from '@radix-ui/themes'
+import { Box, Button, Container, Flex, Separator } from '@radix-ui/themes'
 import styles from './Header.css'
 import { Link } from './Link'
 
 export const Header = () => {
   return (
     <header className={styles.main}>
-      <Link href='/' className={styles.heading}>
-        kakkofn
-      </Link>
+      <Flex style={{width: '100%'}}>
+        <Box style={{width: '200px'}}>
+          <Link href='/' className={styles.heading}>kakkofn</Link>
+        </Box>
+        <Box grow='1' shrink='1'></Box>
+        <Box style={{width: '100px'}}>
+          <Button>edit</Button>
+        </Box>
+      </Flex>
       <Separator size='3' />
     </header>
   )
