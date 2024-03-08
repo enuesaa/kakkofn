06333d73da7ca48651e3e15ed82db45f5098e195
nua <enuesaa@gmail.com>
2024-03-09 01:03:10 +0900

    feat: とりあえず改行trimできるようにした

--- packages/fnui/src/pages/index.tsx
+++ packages/fnui/src/pages/index.tsx
@@ -1,15 +1,27 @@
 import { Header } from '@/components/common/Header'
-import { Box, Container, Flex, Tabs, TextArea } from '@radix-ui/themes'
+import { Box, Button, Container, Flex, Tabs, TextArea, TextField } from '@radix-ui/themes'
+import { MouseEventHandler, useRef } from 'react'
 
 export default function Page() {
+  const fromRef = useRef<HTMLTextAreaElement>(null)
+  const toRef = useRef<HTMLTextAreaElement>(null)
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.preventDefault()
+    if (fromRef.current === null || toRef.current === null) {
+      return
+    }
+
+    toRef.current.value = fromRef.current.value.replaceAll('\n', '')
+  }
+
   return (
     <>
       <Header />
       <Container size='4'>
-        {/* show control menu left size on edit mode */}
         <Flex gap='5' style={{ width: '100%' }}>
           <Box grow='1' shrink='1' style={{ width: '49%' }}>
-            <TextArea size='3' style={{minHeight: '350px'}} />
+            <TextArea size='3' style={{minHeight: '350px'}} ref={fromRef} />
           </Box>
           <Box grow='1' shrink='1' style={{ width: '49%' }}>
             <Flex gap='2'>
@@ -23,6 +35,9 @@ export default function Page() {
           </Box>
         </Flex>
         bind json
+  
+        <Button onClick={handleClick}>改行trim</Button>
+        <TextArea placeholder='改行なし' mt='2' style={{height: '300px'}} ref={toRef} />
       </Container>
     </>
   )
