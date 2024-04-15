a3113601be2e81263300b5cee00d7197a56f161e
nua <enuesaa@gmail.com>
2024-04-16 03:01:46 +0900

    feat(ui): add json format component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import Checkbox from './Checkbox.svelte'
+	import JsonFormat from './JsonFormat.svelte'
 </script>
 
 <h1>hello</h1>
 <Checkbox />
+<JsonFormat />
 
 <style>
 	h1 {
---
+++ src/routes/JsonFormat.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	let text = '';
+	let text2 = ''; // "{text}aa"
+
+	$: {
+		try {
+			text2 = JSON.stringify(JSON.parse(text), null, '  ')
+		} catch (e) {}
+	}
+</script>
+
+<section>
+	<textarea
+		bind:value={text}
+		class="h-40 w-[240px] rounded bg-white px-3 py-2 block"
+	/>
+	<textarea
+		bind:value={text2}
+		class="h-40 w-[240px] rounded bg-white px-3 py-2 block"
+	/>
+</section>
