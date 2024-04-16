35aea9cc427216023e17c1d1e627f77129379540
nua <enuesaa@gmail.com>
2024-04-17 01:58:06 +0900

    feat(count): add text length count page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,4 +4,5 @@
 
 <div class="flex my-2 p-2">
 	<FnCard href="/fns/jsonformat" title="JSON Format" />
+	<FnCard href="/fns/count" title="文字数カウント" />
 </div>
---
+++ src/routes/fns/count/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	let text = ''
+	let count: number = 0
+	$: count = text.length
+</script>
+
+<section>
+	<h3 class="text-4xl font-bold m-1 text-center w-full">文字数カウント</h3>
+
+	<div class="flex mt-7">
+		<div class="flex-1">
+			<textarea
+				bind:value={text}
+				class="mx-auto w-11/12 rounded px-3 py-2 block h-80 bg-grayer text-black outline-none border-black border"
+				placeholder="aaa"
+			/>
+		</div>
+		<div class="flex-1 text-5xl pl-5 pt-5">
+			{count}
+		</div>
+	</div>
+</section>
