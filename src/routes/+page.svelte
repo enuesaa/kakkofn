0cc013ad42c8b0e8189f2e1e8f1da1a20177522b
nua <enuesaa@gmail.com>
2024-04-21 23:15:31 +0900

    feat(fns): add `base64encode`

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,5 +6,6 @@
 	<FnCard href="/fns/jsonformat" title="JSON Format" />
 	<FnCard href="/fns/count" title="文字数カウント" />
 	<FnCard href="/fns/replace" title="文字列置き換え" />
+	<FnCard href="/fns/base64encode" title="Base64 Encode" />
 	<FnCard href="/fns/base64decode" title="Base64 Decode" />
 </div>
---
+++ src/routes/fns/base64encode/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import CopyButton from '$lib/components/CopyButton.svelte'
+	import FnPageLayout from '$lib/components/FnPageLayout.svelte'
+	import FnTextarea from '$lib/components/FnTextarea.svelte'
+	import { Buffer } from 'buffer'
+
+	let text = ''
+	let text2 = ''
+	$: text2 = Buffer.from(text).toString('base64')
+</script>
+
+<FnPageLayout title="Base64 Encode">
+	<svelte:fragment slot="left">
+		<FnTextarea bind:value={text} placeholder="あああ" label="入力" />
+	</svelte:fragment>
+
+	<svelte:fragment slot="right">
+		<FnTextarea bind:value={text2} placeholder="44GC44GC44GC" readonly label="出力" />
+		<CopyButton text={text2} />
+	</svelte:fragment>
+</FnPageLayout>
