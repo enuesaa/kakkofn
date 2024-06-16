78881c1944fe821e66f5c00dc2762cfc1611fbdd
nua <enuesaa@gmail.com>
2024-06-16 23:56:42 +0900

    feat(fns): add remove-returnline

---
+++ src/routes/fns/remove-returnline/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import FnPageLayout from '../FnPageLayout.svelte'
+	import FnTextarea from '$lib/components/FnTextarea.svelte'
+	import CopyButton from '$lib/components/CopyButton.svelte'
+
+	let text = ''
+	let text2 = ''
+	$: {
+		text2 = text.replaceAll('\n', '')
+	}
+</script>
+
+<FnPageLayout title="改行削除">
+	<svelte:fragment slot="left">
+		<FnTextarea bind:value={text} placeholder="aaa" label="テキスト" />
+	</svelte:fragment>
+
+	<svelte:fragment slot="right">
+		<FnTextarea bind:value={text2} placeholder="aaa" readonly label="出力" />
+		<CopyButton text={text2} />
+	</svelte:fragment>
+</FnPageLayout>
