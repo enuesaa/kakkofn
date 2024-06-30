f6cf665a2a9a2568abd8008a84462ba884eefc7d
nua <enuesaa@gmail.com>
2024-07-01 01:38:37 +0900

    chore: remove unused test.

--- src/lib/components/CopyButton.test.ts
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/svelte'
-import { expect, test } from 'vitest'
-import CopyButton from './CopyButton.svelte'
-
-test('CopyButton snapshot', () => {
-  const { container } = render(CopyButton)
-	expect(container).toMatchSnapshot()
-})
