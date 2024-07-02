e5d677a4bec9b12cfa07c8b0de231ff63223af7e
nua <enuesaa@gmail.com>
2024-07-03 01:11:57 +0900

    chore(e2e): add paths to test

--- e2e/pages.ts
+++ e2e/pages.ts
@@ -6,4 +6,9 @@ type Page = {
 	path: string
 }
 
-export const pages: Page[] = [{ name: 'top', path: '/' }]
+export const pages: Page[] = [
+	{ name: 'top', path: '/' },
+	{ name: 'jsonformat', path: '/fns/jsonformat' },
+	{ name: 'count', path: '/fns/count' },
+	{ name: 'replace', path: '/fns/replace' },
+]
