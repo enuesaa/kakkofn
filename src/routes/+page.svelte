3e77830c79a87d4abb0ddcea5ae73a61d72b5261
nua <enuesaa@gmail.com>
2024-07-03 01:16:50 +0900

    feat(top): サイトトップのリンクの並びを調整。vrtが機能しているか確認する意味も込めている

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,14 +6,14 @@
 	<FnCard href="/fns/jsonformat" title="JSON Format" />
 	<FnCard href="/fns/count" title="文字数カウント" />
 	<FnCard href="/fns/replace" title="文字列置き換え" />
+	<FnCard href="/fns/text-search-count" title="文字列の登場回数" />
+	<FnCard href="/fns/remove-linebreak" title="改行削除" />
 	<FnCard href="/fns/base64encode" title="Base64 Encode" />
 	<FnCard href="/fns/base64decode" title="Base64 Decode" />
 	<FnCard href="/fns/urlencode" title="Url Encode" />
 	<FnCard href="/fns/urldecode" title="Url Decode" />
-	<FnCard href="/fns/colorcode-to-rgb" title="カラーコードからRGBへ" />
-	<FnCard href="/fns/rgb-to-colorcode" title="RGBからカラーコードへ" />
 	<FnCard href="/fns/unicode-escape" title="Unicode エスケープ" />
 	<FnCard href="/fns/unicode-unescape" title="Unicode アンエスケープ" />
-	<FnCard href="/fns/text-search-count" title="文字列の登場回数" />
-	<FnCard href="/fns/remove-linebreak" title="改行削除" />
+	<FnCard href="/fns/colorcode-to-rgb" title="カラーコードからRGBへ" />
+	<FnCard href="/fns/rgb-to-colorcode" title="RGBからカラーコードへ" />
 </div>
