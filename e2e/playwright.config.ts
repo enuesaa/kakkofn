c848e0ee1fc334196be060f233c7a6514a77d7ab
nua <enuesaa@gmail.com>
2024-09-10 23:48:10 +0900

    chore(e2e): 依存パッケージをまとめる目的で、e2e テストをサブディレクトリに切り出しパッケージを分ける手法があるらしく、課題感に共感するので試してみる

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -4,24 +4,25 @@ export default defineConfig({
 	use: {
 		browserName: 'firefox',
 	},
-	outputDir: 'e2e/pw-results',
-	snapshotPathTemplate: '{testDir}/e2e/pw-screenshots/{arg}{ext}',
+	outputDir: 'pw-results',
+	snapshotPathTemplate: '{testDir}/pw-screenshots/{arg}{ext}',
 	reporter: [
 		[
 			'html',
 			{
 				open: 'never',
-				outputFolder: 'e2e/pw-report',
+				outputFolder: 'pw-report',
 			},
 		],
 		[
 			'json',
 			{
-				outputFile: 'e2e/pw-results/results.json',
+				outputFile: 'pw-results/results.json',
 			},
 		],
 	],
 	webServer: {
+		cwd: '../',
 		command: 'pnpm run dev',
 		url: 'http://localhost:3000',
 	},
